d26e88586e4d0cf9abd8c2ab0142feae2837627f
Alex <69764315+Serial-ATA@users.noreply.github.com>
2025-02-21 19:27:55 -0500

    chore: update to latest sdk (#43)

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,7 +1,6 @@
 use blueprint_sdk::logging;
 use blueprint_sdk::testing::tempfile;
 use blueprint_sdk::testing::utils::harness::TestHarness;
-use blueprint_sdk::testing::utils::runner::TestEnv;
 use blueprint_sdk::testing::utils::tangle::blueprint_serde::to_field;
 use blueprint_sdk::testing::utils::tangle::TangleTestHarness;
 use blueprint_sdk::tokio;
@@ -16,38 +15,38 @@ async fn test_blueprint() -> color_eyre::Result<()> {
 	let harness = TangleTestHarness::setup(temp_dir).await?;
 	let env = harness.env().clone();
 
-	// Create blueprint-specific context
-	let blueprint_ctx = ServiceContext {
-		config: env.clone(),
-		call_id: None,
-	};
-
-	// Initialize event handler
-	let handler = SayHelloEventHandler::new(&env.clone(), blueprint_ctx)
-		.await
-		.unwrap();
-
 	// Setup service
-	let (mut test_env, service_id) = harness.setup_services().await?;
-	test_env.add_job(handler);
+	let (mut test_env, service_id, _) = harness.setup_services::<1>(false).await?;
+	test_env.initialize().await?;
 
-	tokio::spawn(async move {
-		test_env.run_runner().await.unwrap();
-	});
+	let handles = test_env.node_handles().await;
+	for handle in handles {
+		let config = handle.gadget_config().await;
+		// Create blueprint-specific context
+		let blueprint_ctx = ServiceContext {
+			config: config.clone(),
+			call_id: None,
+		};
+
+		// Initialize event handler
+		let handler = SayHelloEventHandler::new(&config, blueprint_ctx)
+			.await
+			.unwrap();
+
+		handle.add_job(handler).await;
+	}
+
+	test_env.start().await?;
 
 	// Execute job and verify result
 	let job_inputs = vec![to_field("Alice").unwrap()];
 	let expected_outputs = vec![to_field("Hello, Alice!").unwrap()];
 
-	let results = harness
-		.execute_job(
-			service_id,
-			0,
-			job_inputs,
-			expected_outputs,
-		)
-		.await?;
+	let job = harness.submit_job(service_id, 0, job_inputs).await?;
+
+	let results = harness.wait_for_job_execution(service_id, job).await?;
 
 	assert_eq!(results.service_id, service_id);
+	assert_eq!(results.result, expected_outputs);
 	Ok(())
 }
